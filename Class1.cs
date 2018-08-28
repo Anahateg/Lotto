1bc35ef49a594e7d54f460131bf4c80927a863ee
Ana Hateg Nan <ana.hateg@amdaris.com>
2018-08-28 15:06:39 +0300

    My First Commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loto
+{
+    class Player
+    {
+        public string firstname = "Andrei";
+        public string lastname = "hateg";
+        public int age = 23;
+        public string job = " Developer";
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ana's Lotery!\nPlease enter a number from 1 to 5.\nGood Luck!\n ");
+            
+            Player player1 = new Player();
+            Console.Write("Player 1: ");
+            int numar = int.Parse(Console.ReadLine());
+            Console.Write("Player 2: ");
+            int numar2 = int.Parse(Console.ReadLine());
+            Console.Write("------------------------------------------\n");
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 5);
+            Console.WriteLine("\nThe winning number is: " + dice);
+            if (numar == dice)
+                Console.WriteLine("\nCongratulations Player 1!!!!!");
+            else if (numar2 == dice)
+                Console.WriteLine("\nCongratulations Player 2!!!!!");
+            else
+                Console.WriteLine("\nNo winners :(");
+            Console.ReadKey();
+
+        }
+            
+    }
+    }
+
